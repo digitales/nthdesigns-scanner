ab47a9f4f1792db8510627e7031b0c55c8e9056b
Ross t <ross@nthdesigns.co.uk>
2026-06-30 22:24:34 +0100

    Fix the top navigation width

--- resources/js/Components/ui/Status.jsx
+++ resources/js/Components/ui/Status.jsx
@@ -2,7 +2,7 @@ export default function Status({ kind = 'ready', children }) {
     return (
         <span className={`status ${kind}`}>
             <span className="led" />
-            {children}
+            {children ? <span className="status-label">{children}</span> : null}
         </span>
     );
 }
